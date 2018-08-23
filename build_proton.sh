e92f61ad2c9d6642dc000c4d209faf8be1b6c0e5
Christopher Larson <kergoth@gmail.com>
2018-08-23 13:20:58 -0700

    build_proton: fix hardcoded sdl2/avcodec/avutil paths on darwin
    wine will fail to dlopen the x_audio dll.so files as well as
    dinput.dll.so due to the hardcoded absolute paths, so use @rpath.
    
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- build_proton.sh
+++ build_proton.sh
@@ -288,6 +288,12 @@ function build_wine64
     STRIP="$STRIP" $AMD64_WRAPPER make $JOBS
     INSTALL_PROGRAM_FLAGS="$INSTALL_PROGRAM_FLAGS" STRIP="$STRIP" $AMD64_WRAPPER make install-lib
     INSTALL_PROGRAM_FLAGS="$INSTALL_PROGRAM_FLAGS" STRIP="$STRIP" $AMD64_WRAPPER make prefix="$TOOLS_DIR64" libdir="$TOOLS_DIR64/lib64" dlldir="$TOOLS_DIR64/lib64/wine" install-dev install-lib
+    if [ "$PLATFORM" == "Darwin" ]; then
+        # Fix hardcoded absolute paths to libs we linked against
+        install_name_tool -change "$TOOLS_DIR64/lib/libSDL2-2.0.0.dylib" "@rpath/libSDL2.dylib" "$TOOLS_DIR64/lib64/wine/dinput.dll.so"
+        find "$TOOLS_DIR64/lib64/wine" -name xaudio\*.so -print0 | xargs -0 -n 1 install_name_tool -change "$TOOLS_DIR64/lib/libavutil.56.dylib" "@rpath/libavutil.56.dylib"
+        find "$TOOLS_DIR64/lib64/wine" -name xaudio\*.so -print0 | xargs -0 -n 1 install_name_tool -change "$TOOLS_DIR64/lib/libavcodec.58.dylib" "@rpath/libavcodec.58.dylib"
+    fi
     rm -f "$DST_DIR"/bin/{msiexec,notepad,regedit,regsvr32,wineboot,winecfg,wineconsole,winedbg,winefile,winemine,winepath}
     rm -rf "$DST_DIR/share/man/"
 }
@@ -305,6 +311,12 @@ function build_wine32
     STRIP="$STRIP" $I386_WRAPPER make $JOBS
     INSTALL_PROGRAM_FLAGS="$INSTALL_PROGRAM_FLAGS" STRIP="$STRIP" $I386_WRAPPER make install-lib
     INSTALL_PROGRAM_FLAGS="$INSTALL_PROGRAM_FLAGS" STRIP="$STRIP" $I386_WRAPPER make prefix="$TOOLS_DIR32" libdir="$TOOLS_DIR32/lib" dlldir="$TOOLS_DIR32/lib/wine" install-dev install-lib
+    if [ "$PLATFORM" == "Darwin" ]; then
+        # Fix hardcoded absolute paths to libs we linked against
+        install_name_tool -change "$TOOLS_DIR32/lib/libSDL2-2.0.0.dylib" "@rpath/libSDL2.dylib" "$TOOLS_DIR32/lib/wine/dinput.dll.so"
+        find "$TOOLS_DIR32/lib/wine" -name xaudio\*.so -print0 | xargs -0 -n 1 install_name_tool -change "$TOOLS_DIR32/lib/libavutil.56.dylib" "@rpath/libavutil.56.dylib"
+        find "$TOOLS_DIR32/lib/wine" -name xaudio\*.so -print0 | xargs -0 -n 1 install_name_tool -change "$TOOLS_DIR32/lib/libavcodec.58.dylib" "@rpath/libavcodec.58.dylib"
+    fi
 
     #install 32-bit stuff manually, see
     # https://wiki.winehq.org/Packaging#WoW64_Workarounds
